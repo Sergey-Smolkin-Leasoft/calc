87c0fb18e803aaa1ce86a875063c4ccc7efb141c
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2025-05-03 15:22:21 +0300

    к

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-def create_database():
-    with app.app_context():
-        db.create_all()
-        print("Database created successfully!")
-
-if __name__ == '__main__':
-    create_database()
